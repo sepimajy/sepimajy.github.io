d76e2c4b6aae44986de3a5c91f11e70f3f68bc1e
alswij <tradingalswij@gmail.com>
2023-10-01 08:04:01 +0400

    Inital commit

---
+++ GoldenMetal/general/xmlrpc.php
@@ -0,0 +1,14 @@
+<rsd xmlns="http://archipelago.phrasewise.com/rsd" version="1.0">
+<service>
+<engineName>WordPress</engineName>
+<engineLink>https://wordpress.org/</engineLink>
+<homePageLink>https://goldenmetalco.com</homePageLink>
+<apis>
+<api name="WordPress" blogID="1" preferred="true" apiLink="https://goldenmetalco.com/xmlrpc.php"/>
+<api name="Movable Type" blogID="1" preferred="false" apiLink="https://goldenmetalco.com/xmlrpc.php"/>
+<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://goldenmetalco.com/xmlrpc.php"/>
+<api name="Blogger" blogID="1" preferred="false" apiLink="https://goldenmetalco.com/xmlrpc.php"/>
+<api name="WP-API" blogID="1" preferred="false" apiLink="https://goldenmetalco.com/wp-json/"/>
+</apis>
+</service>
+</rsd>
